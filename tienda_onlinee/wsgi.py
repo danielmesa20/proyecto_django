7946b8c658337476ffc63146e2d7f87762c61432
Daniel Alexander Mesa Esquivel <daniel.mesa@correo.unimet.edu.ve>
2019-12-12 10:18:27 -0400

    Primer commit

---
+++ tienda_onlinee/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for tienda_onlinee project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tienda_onlinee.settings')
+
+application = get_wsgi_application()
