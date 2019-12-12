31684d94527ee9e042ca1b9b7edb12900f5db9bb
Daniel Alexander Mesa Esquivel <daniel.mesa@correo.unimet.edu.ve>
2019-12-12 11:23:38 -0400

    Configuracion static files

--- productos/views.py
+++ productos/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def HOME(request):
+     return render(request, "home.html")
+
+
+# def INFO (request):
+#      return render(request, "home.html", {"fecha": datetime.datetime.now()})
