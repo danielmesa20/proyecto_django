52712e81dd2cc4c9a105f9fc4866ac520ceacca3
Daniel Alexander Mesa Esquivel <daniel.mesa@correo.unimet.edu.ve>
2019-12-12 15:11:29 -0400

    pruebas html

--- productos/views.py
+++ productos/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 def HOME(request):
      return render(request, "home.html")
 
+def INFO(request):
+     return render(request, "test.html")
 
 # def INFO (request):
 #      return render(request, "home.html", {"fecha": datetime.datetime.now()})
